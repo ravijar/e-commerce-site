99e15166e3489c1d8153c36391cb1e0fc96713d8
Prasanjith76 <67494584+Prasanjith76@users.noreply.github.com>
2022-12-31 00:50:10 +0530

    Varient Selection Updated
    Varient Selection updated in ProductPage.php

--- functions/common_functions.php
+++ functions/common_functions.php
@@ -117,6 +117,177 @@ function get_categories(){
     }
 }
 
+
+function    view_product_details(){
+  global $adminconnection;
+
+  // checking whether the category is set or not
+  if(isset($_GET['product_id'])){
+    $product_id = $_GET['product_id'];
+    $select_products = "Select * from product where Product_ID=$product_id";
+    $result_products = mysqli_query($adminconnection, $select_products);
+    while($row_data = mysqli_fetch_assoc($result_products)){
+      $Product_ID = $row_data['Product_ID'];
+      $SKU = $row_data['SKU'];
+      $Title = $row_data['Title'];
+      $Weight = $row_data['Weight'];
+      $Category_ID = $row_data['Category_ID'];
+      $Subcategory_ID = $row_data['Subcategory_ID'];
+      $Image = $row_data['Image'];
+
+    echo " <div class='container mt-5'>
+    <section class='py-5 mb-5 bg-light text-dark' id='info'>
+      <div class='container'>
+        <div class='row justify-content-around align-items-start'>
+          <div class='col-d col-4'>
+            <img
+              src='../Admin/Product_Images/$Image'
+              alt=''
+              class='w-75'
+            />
+          </div>
+          <div class='col-d col-6 text-start d-flex flex-column'>
+            <h2 class='mb-4'>$Title</h2>
+            <div class='lead'>Product Description:</div>
+            <p class='mb-4'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque
+              maxime labore deserunt quasi at autem natus veritatis quia
+              voluptas nam!
+            </p>
+    
+            <div class='mb-3 row'>
+              <div class='lead col'>Select Variant:</div>
+              <div class='list-group p-4 pt-2'>
+
+            ";
+          }
+          $iteration = 0;
+          $select_varients = "Select * from product_variant, variant where product_variant.Variant_ID = variant.Varient_ID AND Product_ID=$product_id";
+          $result_varients = mysqli_query($adminconnection, $select_varients);
+          
+          while($row_data = mysqli_fetch_assoc($result_varients)){
+            $Varient_ID = $row_data['Variant_ID'];
+            $Attribute = $row_data['Attribute'];
+            $Value = $row_data['Value'];
+            if($iteration==0){
+              echo "<a
+              href='#'
+              class='list-group-item list-group-item-action'
+              data-bs-toggle='list'
+              id='variant1'
+            >
+              <div class='d-flex w-100 justify-content-between'>
+                <div>
+                <div class='mb-1'>$Attribute: $Value</div>";
+                $iteration+=1;
+            }
+            elseif($iteration==1){
+              echo "<div class='mb-1'>$Attribute: $Value</div>";
+              $iteration+=1;
+            }
+            elseif($Attribute=='ZPrice'){
+                echo "</div>
+                <span class='lead'>$Value LKR</span>
+              </div>
+            </a>";
+            $iteration=0;
+            }
+
+          }
+  echo "          </div>
+  </div>
+  <div class='ms-auto me-4'>
+    <a href='CartPage.php' class='btn btn-primary'>Add to Cart</a>
+  </div>
+</div>
+</div>
+</div>
+</section>
+</div> ";
+
+  
+
+  
+}  
+}
+
+/*<!-- <div class="container mt-5">
+<section class="py-5 mb-5 bg-light text-dark" id="info">
+  <div class="container">
+    <div class="row justify-content-around align-items-start">
+      <div class="col-d col-4">
+        <img
+          src="../Admin/Product_Images/$Image"
+          alt=""
+          class="w-75"
+        />
+      </div>
+      <div class="col-d col-6 text-start d-flex flex-column">
+        <h2 class="mb-4">$Title</h2>
+        <div class="lead">Product Description:</div>
+        <p class="mb-4">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque
+          maxime labore deserunt quasi at autem natus veritatis quia
+          voluptas nam!
+        </p>
+
+        <div class="mb-3 row">
+          <div class="lead col">Select Variant:</div>
+          <div class="list-group p-4 pt-2">
+            <a
+              href="#"
+              class="list-group-item list-group-item-action"
+              data-bs-toggle="list"
+              id="variant1"
+            >
+              <div class="d-flex w-100 justify-content-between">
+                <div>
+                if()
+                  <div class="mb-1">Storage:</div>
+                  <div class="mb-1">Color:</div>
+                </div>
+                <span class="lead">500 LKR</span>
+              </div>
+            </a>
+            <a
+              href="#"
+              class="list-group-item list-group-item-action"
+              data-bs-toggle="list"
+              id="variant2"
+            >
+              <div class="d-flex w-100 justify-content-between">
+                <div>
+                  <div class="mb-1">Storage:</div>
+                  <div class="mb-1">Color:</div>
+                </div>
+                <span class="lead">1000 LKR</span>
+              </div>
+            </a>
+            <a
+              href="#"
+              class="list-group-item list-group-item-action"
+              data-bs-toggle="list"
+              id="variant2"
+            >
+              <div class="d-flex w-100 justify-content-between">
+                <div>
+                  <div class="mb-1">Storage:</div>
+                  <div class="mb-1">Color:</div>
+                </div>
+                <span class="lead">1000 LKR</span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="ms-auto me-4">
+          <a href="#" class="btn btn-primary">Add to Cart</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</div> -->*/
+/*
 function    view_product_details(){
   global $adminconnection;
 
@@ -202,3 +373,4 @@ function    view_product_details(){
   
 }  
 }
+*/
