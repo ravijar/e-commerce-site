754e8b1632607531cfb42acb5c8e51c9cb95911a
hpravija <116764596+hpravija@users.noreply.github.com>
2023-01-01 20:26:45 +0530

    Working - Quarterly sales report

--- Admin/ReportPage.php
+++ Admin/ReportPage.php
@@ -38,6 +38,37 @@
     $products_dict[$product['Product_ID']]=$product['Title'];
   }
 
+  // quarterly sales report
+  $year = $quarter = '';
+  $show_set = false;
+  if(isset($_POST['show'])){
+    if(count($_POST) == 3){
+      $year = $_POST['year'];
+      $quarter = $_POST['quarter'];
+      $show_set = true;
+    }
+  }
+  $sql =$adminconnection->prepare("select Title,sum(Quantity) as Quantity,sum(Item_Total_Price) as Total
+  from `order`,`cart_item`,`product_variant`,`product` 
+  where `order`.Cart_ID = `cart_item`.Cart_ID
+  and `cart_item`.Variant_ID = `product_variant`.Variant_ID
+  and `product_variant`.Product_ID = `product`.Product_ID
+  and year(Date_Of_Order) = ? and ceil(month(Date_Of_Order)/3) = ?
+  group by `product`.Product_ID") ;
+  $sql->bind_param("ss",$year,$quarter);
+  $sql->execute();
+  $result = $sql->get_result();
+  $report  = array();
+  $sum_Quantity = $sum_Total = 0;
+  while($row = $result->fetch_assoc()){
+    $report[] = $row;
+    $sum_Quantity += $row['Quantity'];
+    $sum_Total += $row['Total'];
+  }
+  if(empty($report)){
+    $show_set = false;
+  }
+
   // product with most number of sales
   $year1 = $month1 = '';
   $search1_set = false;
@@ -134,59 +165,65 @@
             <div class="h4 caption-top text-start mb-4">Quarterly Sales Report</div>
             <table class="table caption-top table-secondary table-bordered">
               <div class="d-flex flex-row">
+              <form class="form-inline" method="POST">
                 <div class="d-flex gap-3">
-                  <div>
-    
+                  <div class="form-group">
                   <select
                     class="form-select mb-3"
                     aria-label="Default select example"
+                    name = "year"
                   >
-                    <option selected>Select Year</option>
+                    <option selected disabled>Select Year</option>
                     <?php for($i=$min_year;$i<=$max_year;$i++): ?>
                       <option value=<?php echo $i; ?>><?php echo $i; ?></option>
                     <?php endfor; ?>
                   </select>
                   </div>
-                  <div>
+                  <div class="form-group">
     
                   <select
                     class="form-select mb-3"
                     aria-label="Default select example"
+                    name="quarter"
                   >
-                    <option selected>Select Quarter</option>
+                    <option selected disabled>Select Quarter</option>
                     <option value="1">First Quater</option>
                     <option value="2">Second Quater</option>
-                    <option value="1">Third Quater</option>
-                    <option value="2">Fourth Quater</option>
+                    <option value="3">Third Quater</option>
+                    <option value="4">Fourth Quater</option>
                   </select>
                   </div>
                 </div>
-                <div class="ms-3">
-                  <button class="btn btn-warning">Show</button>
+                <div class="ms-3" class="form-group">
+                  <button class="btn btn-warning" name = "show">Show</button>
                 </div>
+              </form>
               </div>
+              <?php if($show_set): ?>
               <thead>
                 <tr>
-                  <th scope="col">Date</th>
-                  <th scope="col">Variant</th>
+                  <th scope="col">Product</th>
                   <th scope="col">Quantity</th>
-                  <th scope="col">Price</th>
+                  <th scope="col">Total</th>
                 </tr>
               </thead>
               <tbody>
+              <?php foreach($report as $record): ?>
                 <tr>
-                  <th scope="row">1</th>
-                  <td>I Phone</td>
-                  <td>100</td>
-                  <td>2</td>
+                  <td><?php echo $record['Title'];?></td>
+                  <td><?php echo $record['Quantity'];?></td>
+                  <td><?php echo $record['Total'];?></td>
                 </tr>
+                <?php endforeach; ?>
                 <tr>
-                  <th scope="row">1</th>
-                  <td>I Phone</td>
-                  <td>100</td>
-                  <td>2</td>
+                  <th>Total</th>
+                  <th><?php echo $sum_Quantity;?></th>
+                  <th><?php echo number_format((float)$sum_Total, 2, '.', '');?></th>
                 </tr>
               </tbody>
+              <?php else: ?>
+              <span class="text-warning fw-bold"><?php echo 'No available data for the selected period!'; ?><span> 
+              <?php endif; ?>
             </table>
           </div>
     
