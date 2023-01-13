28daaba25ad887664e6367df5cc26946223adad7
Prasanjith76 <67494584+Prasanjith76@users.noreply.github.com>
2023-01-14 00:26:35 +0530

    Updated footer

--- client/inc/footer.php
+++ client/inc/footer.php
@@ -1,7 +1,7 @@
 </div>
 <footer class="bg-dark text-center text-light mt-auto position-static">
     <div class="text-center p-3 text-secondary" style="background-color: rgba(0, 0, 0, 0.3)">
-        Copyright &copy; 2022 My Shop
+        Copyright &copy; 1998-2022, Kade.lk
     </div>
 </footer>
 </body>
